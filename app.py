955583d4faa073507b11e4bc71833bc645d2d864
demidraksin <greatraksin@icloud.com>
2021-05-04 11:44:49 +0300

    Adding db

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 from forms import ContactForm
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'try-to-guess'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
